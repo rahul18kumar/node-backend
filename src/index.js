ff780a3018bbffabcae335cb5d97e9da5a1d937d
rahul18kumar <rk4955528@gmail.com>
2026-03-27 15:31:03 +0530

    connect mongoDB Atlas

---
+++ src/index.js
@@ -0,0 +1,49 @@
+// import 'dotenv/config'
+
+import dotenv from "dotenv";
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// First approach of connecting database through iife
+
+// import express from "express";
+// const app = express();
+
+// ;(async ()=>{
+//  try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//     app.on("error",(error)=>{
+//         console.log("Error", error);
+//         throw error
+//     })
+
+//     app.listen(process.env.PORT,()=>{
+//         console.log(`Ap is listening on port ${process.env.PORT}`)
+//     })
+
+// } catch (error) {
+//     console.log("Error:",error)
+//     throw error
+//  }
+// })()
+
+// connectDB()

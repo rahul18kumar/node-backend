f4accee13b18e39e9ad45458120d0ca0244517c4
rahul18kumar <rk4955528@gmail.com>
2026-03-29 19:41:46 +0530

    add controllers,routes basics for testing

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,4 +1,4 @@
-const asyncHandler = (requestHandler)=> (req, res, next) {
+const asyncHandler = (requestHandler) => (req, res, next) => {
     Promise.resolve(requestHandler(req,res,next))
     .catch((err)=>next(err))
 }
@@ -12,13 +12,13 @@ export {asyncHandler}
 // const asyncHandler =(func)=> async ()=>{}
 
 
-const asyncHandler = (fn)=> async (req,res,next) => {
-    try {
-        await fn(req,res,next)
-    } catch (err) {
-        res.status(err.code || 500).json({
-            success: false,
-            messege: err.messege
-        })
-    }
-}
+// const asyncHandler = (fn)=> async (req,res,next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (err) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             messege: err.messege
+//         })
+//     }
+// }
